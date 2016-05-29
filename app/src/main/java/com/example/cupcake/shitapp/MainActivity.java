d6569aa1eb63ddcce1b2535bf9e7f307054d64f2
陳柏翰 <cupcake@chenbohande-MacBook-Air.local>
2016-05-30 01:34:41 +0800

    Add toolbar

--- app/src/main/java/com/example/cupcake/shitapp/MainActivity.java
+++ app/src/main/java/com/example/cupcake/shitapp/MainActivity.java
@@ -3,14 +3,24 @@ package com.example.cupcake.shitapp;
 import android.app.Activity;
 import android.preference.PreferenceFragment;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
 
 public class MainActivity extends Activity {
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initToolBar();
         getFragmentManager().beginTransaction()
                 .replace(R.id.llPF, new PrefsFragment()).commit();
+    }
+
+    public void initToolBar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Fastest Wifi");
+        toolbar.setTitleTextColor(0xFFFFFFFF);
     }
 
     public static class PrefsFragment extends PreferenceFragment {
