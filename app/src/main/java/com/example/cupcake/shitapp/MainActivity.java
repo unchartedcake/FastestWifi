c36d271e5a7b1163452e79f24cce42d9f7ced35f
MROS <yc1043@gmail.com>
2016-06-01 01:27:38 +0800

    顯示目前WIFI名稱

--- app/src/main/java/com/example/cupcake/shitapp/MainActivity.java
+++ app/src/main/java/com/example/cupcake/shitapp/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.cupcake.shitapp;
 
 import android.app.Activity;
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.preference.PreferenceFragment;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
@@ -12,6 +16,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView wifiNow = (TextView) findViewById(R.id.tvWifi);
+        wifiNow.setText("你的網路是 " + getSSID());
         initToolBar();
         getFragmentManager().beginTransaction()
                 .replace(R.id.llPF, new PrefsFragment()).commit();
@@ -21,6 +27,12 @@ public class MainActivity extends Activity {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("Fastest Wifi");
         toolbar.setTitleTextColor(0xFFFFFFFF);
+    }
+
+    public String getSSID() {
+        WifiManager wifiManager = (WifiManager) getSystemService (Context.WIFI_SERVICE);
+        WifiInfo info = wifiManager.getConnectionInfo ();
+        return info.getSSID ();
     }
 
     public static class PrefsFragment extends PreferenceFragment {
