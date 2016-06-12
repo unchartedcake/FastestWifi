0e4ab538ec3ec5f0bf0cde13a66610d19b6fdd24
Eug83 <Eug112358@gmail.com>
2016-06-12 12:53:37 +0800

    add change network

--- app/src/main/java/com/example/cupcake/shitapp/MainActivity.java
+++ app/src/main/java/com/example/cupcake/shitapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.wifi.ScanResult;
+import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Build;
@@ -41,18 +42,40 @@ public class MainActivity extends Activity {
             wm.setWifiEnabled(true);
         }
         requestPermissions(new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION,android.Manifest.permission.ACCESS_FINE_LOCATION},0);
-        wm.startScan();
+        /*wm.startScan();
         List<ScanResult> pList=wm.getScanResults();
         String result="There are "+pList.size()+" wifi\n";
         for(int i = 0 ; i < pList.size() ; i++){
             result=result+pList.get(i).SSID+"\n";
+        }*/
+        //wi=wm.getConnectionInfo();
+        //result=result+"Current link speed= "+wi.getLinkSpeed()+"\n";
+        String result="";
+        wm.disconnect();
+        int netId=0;
+        int speed=1-;
+        String wifi="";
+        List<WifiConfiguration> wcList=wm.getConfiguredNetworks();
+        for(int i = 0 ; i < wcList.size();i++){
+            WifiConfiguration w=wcList.get(i);
+            int nid=w.networkId;
+            String ssid=w.SSID;
+            wm.enableNetwork(nid,false);
+            wi=wm.getConnectionInfo();
+            int currSpeed=wi.getLinkSpeed();
+            result=result+"Wifi "+i+":"+ssid+"\n"+"netId="+nid+",speed="+currSpeed+"\n";
+            if(currSpeed > speed){
+                netId=nid;
+                speed=currSpeed;
+                wifi=ssid;
+            }
+            wm.disconnect();
         }
-        wi=wm.getConnectionInfo();
-        result=result+"Current link speed= "+wi.getLinkSpeed()+"\n";
+        wm.enableNetwork(netId,true);
+        result=result+"Connect to "+wifi+"\n";
         TextView test=(TextView) findViewById(R.id.debug);
         test.setText(result);
     }
-
     public void initToolBar() {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("Fastest Wifi");
